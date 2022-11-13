f1d27344746e36435655b9950177a752b2059a6d
takashi0602 <hi_takahashi@yumemi.co.jp>
2022-11-13 19:26:45 +0900

    feat: Closeボタン押下時に前のページへ戻る

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -66,7 +66,11 @@ class HomePage extends ConsumerWidget {
                         Expanded(
                           child: Center(
                             child: TextButton(
-                              onPressed: () => {},
+                              onPressed: () {
+                                if (Navigator.of(context).canPop()) {
+                                  Navigator.of(context).pop();
+                                }
+                              },
                               child: const Text('Close'),
                             ),
                           ),
