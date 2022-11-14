17de277f1e6a16e64d0e5d20f20bc2fc20d7f094
takashi0602 <hi_takahashi@yumemi.co.jp>
2022-11-14 16:21:58 +0900

    refactor: 不要なコードを削除

--- lib/ui/pages/first_page.dart
+++ lib/ui/pages/first_page.dart
@@ -3,14 +3,11 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class FirstPage extends HookConsumerWidget with WidgetsBindingObserver {
+class FirstPage extends HookConsumerWidget {
   const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final TextStyle? textThemeLabelLarge =
-        Theme.of(context).textTheme.labelLarge;
-
     // 画面を描画後、0.5秒待機し画面遷移する
     useEffect(() {
       WidgetsBinding.instance.endOfFrame.then(
@@ -24,83 +21,6 @@ class FirstPage extends HookConsumerWidget with WidgetsBindingObserver {
       return null;
     }, const []);
 
-    return Scaffold(
-      body: SizedBox.expand(
-        child: FractionallySizedBox(
-          widthFactor: 0.5,
-          alignment: FractionalOffset.center,
-          child: Column(
-            children: [
-              const Spacer(
-                flex: 1,
-              ),
-              Column(
-                children: [
-                  const AspectRatio(
-                    aspectRatio: 1,
-                    child: Placeholder(),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          '** ℃',
-                          textAlign: TextAlign.center,
-                          style:
-                              textThemeLabelLarge?.copyWith(color: Colors.blue),
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          '** ℃',
-                          textAlign: TextAlign.center,
-                          style:
-                              textThemeLabelLarge?.copyWith(color: Colors.red),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                ],
-              ),
-              Flexible(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 80,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Center(
-                            child: TextButton(
-                              onPressed: () => {},
-                              child: const Text('Close'),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Center(
-                            child: TextButton(
-                              onPressed: () => {},
-                              child: const Text('Reload'),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return const Scaffold();
   }
 }
