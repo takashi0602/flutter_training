19519699810818c4de0df9664be8b9caa9f3ee90
takashi0602 <hi_takahashi@yumemi.co.jp>
2022-11-16 08:33:50 +0900

    fix: 生成したコードを用いてFirstページの処理を修正する

--- lib/ui/pages/first_page.dart
+++ lib/ui/pages/first_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_training/router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,15 +10,16 @@ class FirstPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = GoRouter.of(context);
+    final firstRouteLocation = const FirstRoute().location;
 
     // 画面を描画後、0.5秒待機し画面遷移する
     useEffect(() {
-      if (router.location != '/first') return;
+      if (router.location != firstRouteLocation) return;
 
       WidgetsBinding.instance.endOfFrame.then(
         (_) {
           Future.delayed(const Duration(milliseconds: 500), () {
-            context.push('/home');
+            const HomeRoute().push(context);
           });
         },
       );
